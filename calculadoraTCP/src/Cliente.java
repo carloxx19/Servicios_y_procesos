9c64f23ea78fc4831070735f3c9545190c03e16c
carloxx19 <carlosrtoledo.133@gmail.com>
2022-12-20 17:30:19 +0100

    CalculadoraTCP

---
+++ calculadoraTCP/src/Cliente.java
@@ -0,0 +1,81 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Cliente {
+    public static void main(String[] args) throws IOException {
+
+        //VARIABLES
+        final String HOST = "localhost";
+        final int PUERTO = 5001;
+        int operacion;
+        Scanner teclado = new Scanner(System.in);
+        Socket socket = new Socket(HOST, PUERTO);
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+        String mensaje = in.readUTF();
+
+        while (true) {
+            try {
+                System.out.println(mensaje);
+                operacion = teclado.nextInt();
+                if (operacion <= 0 || operacion > 5) {
+                    System.err.println("un numero del 1 al 5");
+                } else {
+                    out.writeInt(operacion);
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.err.println("INTRODUCE UN NUMERO NO LETRA!!!!");
+                //Esta linea arregla el problema de: Cuando se reinicia el bucle se hace de forma infinita y no deja
+                //introducir datos, lo que hace es "vaciar el buffer permitiendo la entrada de datos otra vez"
+                teclado.nextLine();
+            }
+        }
+
+        //PIRMER NUMERO (se aplica de la misma forma para el segundo numero)
+        //recibir primera respuesta
+        mensaje = in.readUTF();
+
+        while (true) {
+            try {
+                System.out.println(mensaje);
+                //Introducimos el numero a enviar al servidor
+                int num = teclado.nextInt();
+                out.writeInt(num);
+                break;
+            } catch (InputMismatchException e) {
+                System.err.println("INTRODUCE UN NUMERO NO LETRA!!!!");
+                teclado.nextLine();
+            }
+        }
+
+        //SEGUNDO NUMERO
+        mensaje = in.readUTF();
+
+        while (true) {
+            try {
+                System.out.println(mensaje);
+                int num = teclado.nextInt();
+                out.writeInt(num);
+                break;
+            } catch (InputMismatchException e) {
+                System.err.println("INTRODUCE UN NUMERO NO LETRA!!!!");
+                teclado.nextLine();
+            }
+        }
+
+        //RECIBIR RESULTADO
+        double resultado = in.readDouble();
+        String cero = "NaN";
+        if (cero.equalsIgnoreCase(String.valueOf(resultado))) {
+            System.out.println("Al dividir 0 el resultado siempre va a ser: 0");
+        } else {
+            System.out.println("El resultado es: " + resultado);
+        }
+    }
+}
---
+++ calculadoraTCP/src/Servidor.java
@@ -0,0 +1,87 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+    static int numero1;
+    static int numero2;
+    static double resultado;
+
+    public static void main(String[] args) throws IOException {
+        //VARIABLES
+        final int PORT = 5001;
+        Socket socket = null;
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        DataInputStream in;
+        DataOutputStream out;
+
+        while (true) {
+            socket = serverSocket.accept();
+            String mensaje = """
+                    ¿Que operacion desea realizar?
+                    1.-sumar.
+                    2.-restar.
+                    3.-multiplicar.
+                    4.-dividir.
+                    """;
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            out.writeUTF(mensaje);
+
+            int operacion = in.readInt();
+
+            switch (operacion) {
+                case 1 -> {
+                    pedirNumeros(out, in);
+                    resultado = numero1 + numero2;
+                    out.writeDouble(resultado);
+
+                }
+                case 2 -> {
+                    pedirNumeros(out, in);
+                    resultado = numero1 - numero2;
+                    out.writeDouble(resultado);
+
+                }
+                case 3 -> {
+                    pedirNumeros(out, in);
+                    resultado = numero1 * numero2;
+                    out.writeDouble(resultado);
+
+                }
+                case 4 -> {
+                    try {
+                        pedirNumeros(out, in);
+                        //tenemos que hacer Cast a un numero entero a double para que guarde los decimanes, si no va a dar 0
+                        //aunque el resultado de dividir 2 enteros sea double con decimales.
+                        resultado = (double) numero1 / numero2;
+                        System.out.println(resultado);
+                        out.writeDouble(resultado);
+
+                    } catch (ArithmeticException arithmeticException) {
+                        System.out.println("Estas diviendo 0 y el resultado es:");
+                        resultado = 0;
+                        out.writeDouble(resultado);
+                    }
+                }
+                default -> System.out.println("numero introducido erroneo");
+            }
+        }
+    }
+
+    public static void pedirNumeros(DataOutputStream outputStream, DataInputStream inputStream) throws IOException {
+
+        //primer numero
+        String mensaje = "Introduce el primer numero";
+        outputStream.writeUTF(mensaje);
+        numero1 = inputStream.readInt();
+
+        //segundo numero
+        mensaje = "Introduce el segundo numero";
+        outputStream.writeUTF(mensaje);
+        numero2 = inputStream.readInt();
+    }
+}
